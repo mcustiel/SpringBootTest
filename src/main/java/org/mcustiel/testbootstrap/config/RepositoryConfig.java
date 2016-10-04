64e6de4b0ca5b92d885519850cdd737dd522a569
Mariano Custiel <mcustiel@goodgamestudios.com>
2016-10-04 09:22:17 +0200

    Added datasource

--- src/main/java/org/mcustiel/testbootstrap/config/RepositoryConfig.java
+++ src/main/java/org/mcustiel/testbootstrap/config/RepositoryConfig.java
@@ -1,15 +1,42 @@
 package org.mcustiel.testbootstrap.config;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
 import org.mcustiel.testbootstrap.repository.DashboardDao;
 import org.mcustiel.testbootstrap.repository.implementation.InMemoryDashboardDao;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
+@EnableJpaRepositories
 public class RepositoryConfig {
-    @Bean
-    public DashboardDao getDashboardDao() {
-	return new InMemoryDashboardDao();
-    }
+	@Bean
+	public DashboardDao getDashboardDao() {
+		return new InMemoryDashboardDao();
+	}
 
+	@Bean
+	public DataSource dataSource() {
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		return builder.setType(EmbeddedDatabaseType.HSQL).build();
+	}
+
+	@Bean
+	public EntityManagerFactory entityManagerFactory() {
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setGenerateDdl(true);
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setJpaVendorAdapter(vendorAdapter);
+		//factory.setPackagesToScan("org.mcustiel.testbootstrap.repository");
+		factory.setDataSource(dataSource());
+		factory.afterPropertiesSet();
+
+		return factory.getObject();
+	}
 }
